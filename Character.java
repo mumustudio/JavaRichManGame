3ae8fe0ddbfabc7e788e90fc300af100728d1467
Lin_Yi_Shen <44767582+MuMuShy@users.noreply.github.com>
2020-11-10 22:26:59 +0800

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,18 @@
+
+public class Character {
+	int location;
+	int CHARACTER_NUMBER;
+	int money;
+	int status;
+	String IMAGE_FILENAME;
+	
+
+	Character(){
+	  this.location = 0;
+	  this.CHARACTER_NUMBER = 0;
+	  this.money = 0;
+	  this.status = 0;
+	  this.IMAGE_FILENAME ="";
+	}
+
+}
---
+++ Checkpiont1.java
@@ -0,0 +1,22 @@
+
+public class Checkpiont1 {
+	public static void main(String[] args) throws IOException {
+		//// TODO: Declare the object of Character. ////
+		//// Then, Load the information from the "1_Character.txt" file, ////
+		//// and modify the variables which we ask you to do and then Save it. ////
+		//// Finally, output the outcome this section need. ////
+		
+		
+	}
+
+	static void Load(String filename) throws IOException {
+		//// TODO: You should load the variables from the file. ////
+
+	}
+
+	static void Save(String filename) throws IOException {
+		//// TODO: You should save the changed variables into original data (filename) ////
+		
+	}
+
+}
